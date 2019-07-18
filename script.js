c4d09319e5effb832ea418c5f28d31370c8beb7b
m-cal <iengineersoftware@gmail.com>
2019-07-18 15:48:54 -0400

    finished part 1 dog images rendering correctly

--- script.js
+++ script.js
@@ -3,16 +3,35 @@
 /* global $ */
 
 function handleSubmit() {
-  $('.randomDogForm').submit(event => {
+  $('.js-randomDogForm').submit(event => {
     event.preventDefault();
-    const number = $('.numberInput').val();
+    const number = $('.js-numberInput').val();
+    $('.js-dogImagesSection').html('');
     console.log(number);
+    getDogImage(number);
   });
 }
 
+
+function getDogImage(number) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
+    .then(response => response.json())
+    .then(data => renderImages(data))
+    .catch(error => console.log(error)
+    );
+}
+
+function renderImages(data) {
+  for (let i = 0; i < data['message'].length; i++) {
+    $('.js-dogImagesSection').append(`
+    <img src="${data['message'][i]}"></img>
+    `);
+  }
+}
 
 function handleEverything() {
   handleSubmit();
 }
 
 $(handleEverything());
+
