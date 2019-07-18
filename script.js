c3e08bbf7a7d363a50c1adc40e6b3a133d94334d
m-cal <iengineersoftware@gmail.com>
2019-07-18 14:56:30 -0400

    console now logs input number

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+/* global $ */
+
+function handleSubmit() {
+  $('.randomDogForm').submit(event => {
+    event.preventDefault();
+    const number = $('.numberInput').val();
+    console.log(number);
+  });
+}
+
+
+function handleEverything() {
+  handleSubmit();
+}
+
+$(handleEverything());
